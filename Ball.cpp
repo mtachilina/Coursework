6bb6a4c12c7b611affe5c9addea81fd3c5aeb137
mtachilina <tachilina.ma@edu.spbstu.ru>
2026-05-25 13:08:26 +0300

    Update Ball.cpp
    Поменяла скорость шара

--- Ball.cpp
+++ Ball.cpp
@@ -5,7 +5,7 @@ Ball::Ball(float x, float y)
     shape = sf::CircleShape(8.f);
     shape.setPosition(x, y);
     shape.setFillColor(sf::Color::White);
-    vel = sf::Vector2f(2.5f, -2.5f);
+    vel = sf::Vector2f(3.f, -3.f);
 }
 
 void Ball::update()
